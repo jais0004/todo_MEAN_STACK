57b05493633bb41ad0837524d4ed674858869c66
Nitinkumar Jaiswal <nitin089@gmail.com>
2016-09-05 17:24:11 -0400

    client side files

---
+++ client/public/javascripts/app.js
@@ -0,0 +1,60 @@
+angular.module('nodeTodo', [])
+
+.controller('mainController', function($scope, $http) {
+
+    $scope.formData = {};
+    $scope.formData.date = new Date();
+    $scope.todoData = {};
+
+    // Get all todos
+    $http.get('/api/v1/todos')
+        .success(function(data) {
+            $scope.todoData = data;
+            console.log(data);
+        })
+        .error(function(error) {
+            console.log('Error: ' + error);
+        });
+
+    // Create a new todo
+    $scope.createTodo = function(todoID) {
+        $http.post('/api/v1/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.formData.date = new Date();
+                $scope.todoData = data;
+                console.log(data);
+            })
+            .error(function(error) {
+                console.log('Error: ' + error);
+            });
+    };
+
+    // Delete a todo
+    $scope.completeTodo = function(todoID) {
+        $http.get('/api/v1/todos/complete/' + todoID)
+            .success(function(data) {
+                $scope.todoData = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.deleteTodo = function(todoID) {
+      if (confirm("Confirm ! Delete it") == true) {
+
+
+        $http.delete('/api/v1/todos/' + todoID)
+            .success(function(data) {
+                $scope.todoData = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+  }
+
+});
